65c32e6ef1f557f5c319f586bc5c3c3ab5de5394
Onkarjadhav96k <118995949+Onkarjadhav96k@users.noreply.github.com>
2024-04-04 15:21:14 +0530

    Update MergeArrays.java

--- MergeArrays.java
+++ MergeArrays.java
@@ -1,20 +1,19 @@
 public class MergeArrays {
 
   public static void merge(int[] X, int m, int[] Y, int n) {
-    // Find the index of the last non-vacant cell in X
+   
     int i = m - 1;
 
     // Merge Y into X starting from the end
     int j = n - 1;
     while (i >= 0 && j >= 0) {
       if (X[i] > Y[j]) {
-        X[i--] = X[i]; // Shift non-vacant elements of X one position back
+        X[i--] = X[i]; 
       } else {
-        X[i--] = Y[j--]; // Insert Y[j] into the vacant cell in X
+        X[i--] = Y[j--]; 
       }
     }
 
-    // Copy the remaining elements of Y, if any
     while (j >= 0) {
       X[i--] = Y[j--];
     }
